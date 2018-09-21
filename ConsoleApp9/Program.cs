370e13a31673891d55d87c0886a4bf62ce76ca71
Micro Macro Bitbucket Admin <vu@micromacro.local>
2018-09-21 11:43:35 +1000

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "";
+            List<string> list = new List<string>() { "", "test"};
+
+            if (list.Contains(test))
+            {
+                string test2 = "";
+            }
+            else
+            {
+                string test3 = "";
+            }
+
+        }
+    }
+}
